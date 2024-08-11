07ffd3f6e6d69c7a96e789f3b7bd971ceb50aef0
Arya-Kishan <arya12345kishan@gmail.com>
2024-08-11 20:40:08 +0530

    BgChange

--- Grid_Lights/script.js
+++ Grid_Lights/script.js
@@ -3,7 +3,9 @@ let Allboxes = document.querySelectorAll(".box");
 let boxArr = [];
 Allboxes.forEach((e) => {
   e.addEventListener("click", (e) => {
-    e.target.style.background = "red";
+    // e.target.style.background = "red";
+    e.target.style.backgroundImage = "url('../images/bg2.jpg')";
+    e.target.style.backgroundSize = "contain";
     boxArr.push(e.target);
     if (boxArr.length >= Allboxes.length) {
       handleReverseLighting();
@@ -14,14 +16,9 @@ Allboxes.forEach((e) => {
 function handleReverseLighting() {
   boxArr.reverse().forEach((e, i) => {
     setTimeout(() => {
+      // e.style.backgroundImage = "url('../images/bg1.jpg')";
       e.style.background = "transparent";
       if (i == Allboxes.length - 1) boxArr = [];
     }, (i + 1) * 300);
   });
 }
-
-// CHANGE BACKGROUND COLOR
-let c = document.getElementById("colorChange");
-c.addEventListener("change", (e) => {
-  document.body.style.background = e.target.value;
-});
